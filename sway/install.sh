966e2a9180ecf54afc3c6c88b31b9baead4e937f
Jan van Brügge <supermanitu@gmail.com>
2019-11-22 19:11:04 +0100

    waybar: initial config

--- sway/install.sh
+++ sway/install.sh
@@ -6,10 +6,17 @@ if [ -e "$config" ]; then
     rm -r "$config"
 fi
 
+if [ -e "$HOME/.config/waybar" ]; then
+    rm -r "$HOME/.config/waybar"
+fi
+
 mkdir -p "$config/conf.d"
+mkdir -p "$HOME/.config/waybar"
 
 ln -s "$(pwd)/sway/config" "$config/config"
 
 for f in $(ls "$(pwd)/sway/conf.d"); do
     ln -s "$(pwd)/sway/conf.d/$f" "$config/conf.d/"
 done
+
+ln -s "$(pwd)/sway/waybar.json" "$HOME/.config/waybar/config"
