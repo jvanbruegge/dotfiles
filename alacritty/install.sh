37aac00be9e608a19a401c98dc5ee97388fc1bd7
Jan van Brügge <supermanitu@gmail.com>
2019-11-22 18:55:03 +0100

    sway: tidy display configuration

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [ -e "$HOME/.config/alacritty" ]; then
-    rm "$HOME/.config/alacritty"
+    rm -r "$HOME/.config/alacritty"
 fi
 
+mkdir -p "$HOME/.config/alacritty"
 ln -s "$(pwd)/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
--- sway/install.sh
+++ sway/install.sh
@@ -6,6 +6,10 @@ if [ -e "$config" ]; then
     rm -r "$config"
 fi
 
-mkdir -p "$config"
+mkdir -p "$config/conf.d"
 
 ln -s "$(pwd)/sway/config" "$config/config"
+
+for f in $(ls "$(pwd)/sway/conf.d"); do
+    ln -s "$(pwd)/sway/conf.d/$f" "$config/conf.d/"
+done
