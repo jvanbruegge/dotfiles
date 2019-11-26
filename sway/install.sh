c50aa7adf348b2947b1cd0c625b5e3e52a7a6472
Jan van Brügge <supermanitu@gmail.com>
2019-11-26 21:13:36 +0100

    sway: Put wallpapers in git too

--- sway/install.sh
+++ sway/install.sh
@@ -11,12 +11,17 @@ if [ -e "$HOME/.config/waybar" ]; then
 fi
 
 mkdir -p "$config/conf.d"
+mkdir -p "$config/wallpapers"
 mkdir -p "$HOME/.config/waybar"
 
 ln -s "$(pwd)/sway/config" "$config/config"
 
 for f in $(ls "$(pwd)/sway/conf.d"); do
     ln -s "$(pwd)/sway/conf.d/$f" "$config/conf.d/"
+done
+
+for f in $(ls "$(pwd)/sway/wallpapers"); do
+    ln -s "$(pwd)/sway/wallpapers/$f" "$config/wallpapers/"
 done
 
 ln -s "$(pwd)/sway/waybar.json" "$HOME/.config/waybar/config"
