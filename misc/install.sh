b9dc1395e44c244cb483b6d2d1b4ae09285223ca
Jan van Brügge <supermanitu@gmail.com>
2020-01-13 14:12:21 +0100

    general: Automatically switch to HDMI audio

--- misc/install.sh
+++ misc/install.sh
@@ -10,3 +10,8 @@ for ((i=1;i<=4;i++)); do
     ntp="${ntp}server $i.de.pool.ntp.org iburst\n"
 done
 echo -e "$ntp" | sudo tee /etc/ntp.conf > /dev/null
+
+# Automatically switch to HDMI auto when connected
+sudo ln -s "$(pwd)/hdmi_sound_toggle.sh" /usr/local/bin/hdmi_sound_toggle.sh
+echo 'KERNEL=="card0", SUBSYSTEM=="drm", ACTION=="change", RUN+="/usr/local/bin/hdmi_sound_toggle.sh"' | sudo tee /etc/udev/rules.d/99-hdmi_sound.rules
+sudo udevadm control --reload-rules
