39256f6ee99957f56585f38df9cc92e679954fc5
Jan van Brügge <supermanitu@gmail.com>
2020-04-24 11:04:08 +0200

    ssh-agent: Remove unused config

--- ssh-agent/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p "$HOME/.config/systemd/user/"
-cp ./ssh-agent/ssh-agent.service "$HOME/.config/systemd/user/"
-sudo systemctl daemon-reload
-systemctl --user enable ssh-agent.service
-systemctl --user start ssh-agent.service
