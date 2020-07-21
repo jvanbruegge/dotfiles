aeb1935ac5dc2145a2335ecd25d714a7b91bd900
Jan van Brügge <supermanitu@gmail.com>
2020-07-21 10:28:49 +0200

    general: Remove pass

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -4,9 +4,3 @@ mkdir -p "$HOME/.gnupg"
 ln -s "$(pwd)"/gnupg/gpg.conf "$HOME/.gnupg"
 
 gpg --recv-keys 0x366572be7d6c78a2
-
-git clone git@github.com:jvanbruegge/password-store.git "$HOME/.password-store"
-
-cd /usr/lib/browserpass || exit 1
-make configure
-make hosts-firefox-user
